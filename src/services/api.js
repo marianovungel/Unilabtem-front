04d70e6505fd3f4e80982a66c49e11377d00ecd6
marianovungel <vungem88@gmail.com>
2022-10-23 19:52:19 -0300

    servidor AWS-EC2

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://backunilabtem.herokuapp.com/',
+    baseURL: 'http://unilabtemserver-env.eba-wsnv2ibv.us-east-1.elasticbeanstalk.com/',
+    // baseURL: 'https://backunilabtem.herokuapp.com/',
     // baseURL: 'http://localhost:8000/'
 });
 
