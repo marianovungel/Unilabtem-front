cffeffe778f526809a255f0bbe636fc636ac8638
marianovungel <vungem88@gmail.com>
2022-10-26 12:20:57 -0300

    usando o server na heroku

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://unilabtemserver-env.eba-wsnv2ibv.us-east-1.elasticbeanstalk.com/',
-    // baseURL: 'https://backunilabtem.herokuapp.com/',
+    //baseURL: 'http://unilabtemserver-env.eba-wsnv2ibv.us-east-1.elasticbeanstalk.com/',
+    baseURL: 'https://backunilabtem.herokuapp.com/',
     // baseURL: 'http://localhost:8000/'
 });
 
