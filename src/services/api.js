d47adea49927cb0eb0f710a1b17af21049950c2a
marianovungel <vungem88@gmail.com>
2022-10-30 09:52:08 -0300

    troca de URL

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
     //baseURL: 'http://unilabtemserver-env.eba-wsnv2ibv.us-east-1.elasticbeanstalk.com/',
-    //baseURL: 'https://backunilabtem.herokuapp.com/',
-    baseURL: 'http://localhost:8000/'
+    baseURL: 'https://backunilabtem.herokuapp.com/',
+    //baseURL: 'http://localhost:8000/'
 });
 
 export default api;
