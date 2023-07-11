afcb83b604c29ccb7b8b5167fa5a0b124de1c614
marianovungel <vungem88@gmail.com>
2023-07-11 13:54:25 -0300

    new Packahe

--- src/components/VendaEditMonitor/VendaEditMonitor.js
+++ src/components/VendaEditMonitor/VendaEditMonitor.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import { useState } from 'react'
+import { useState } from 'react'
 import { useEffect } from 'react'
 import api from '../../services/api'
 import './VendaEditMonitor.css'
